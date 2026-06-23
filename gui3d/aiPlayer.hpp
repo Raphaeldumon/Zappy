7d78b3623a2701375b5eb4f91df95e753710f8af
heroduneant <clement.fabre@epitech.eu>
2026-06-23 11:19:56 +0400

    player finished

--- gui3d/aiPlayer.hpp
+++ gui3d/aiPlayer.hpp
@@ -12,8 +12,8 @@ enum class Orientation : std::uint8_t
 class aiPlayer
 {
 private:
-    std::uint32_t _id;
-    std::string _team;
+    const std::uint32_t _id;
+    const std::string _team;
     int x{};
     int y{};
     Orientation orientation{Orientation::North};
@@ -23,4 +23,37 @@ private:
 public:
     aiPlayer(std::uint32_t id, const std::string& team, int x, int y, Orientation orientation) : _id(id), _team(team), x(x), y(y), orientation(orientation) {};
     ~aiPlayer();
+    void changePosition(int newX, int newY) {
+        x = newX;
+        y = newY;
+    };
+    void changeOrientation(Orientation newOrientation) {
+        orientation = newOrientation;
+    };
+    void changeLevel(int newLevel) {
+        level = newLevel;
+    };
+    void changeLifeUnits(int newLifeUnits) {
+        life_units = newLifeUnits;
+        if (life_units <= 0) {
+            alive = false;
+        }
+    };
+    void setAlive(bool isAlive) {
+        alive = isAlive;
+    };
+    void setLifeUnits(int newLifeUnits) {
+        life_units = newLifeUnits;
+    };
+    void setLevel(int newLevel) {
+        level = newLevel;
+    };
+    std::uint32_t getId() const {return _id;};
+    std::string getTeam() const {return _team;};
+    int getX() const {return x;};
+    int getY() const {return y;};
+    Orientation getOrientation() const {return orientation;};
+    int getLevel() const {return level;};
+    int getLifeUnits() const {return life_units;};
+    bool isAlive() const {return alive;};
 };
